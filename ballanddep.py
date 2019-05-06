d9fc39b73975071efb258168593080c7b0eaadc2
APPASEYO <yoho64@outlook.fr>
2019-05-06 08:32:51 +0200

    Ajout de la fonction Distance et Tirer

--- ballanddep.py
+++ ballanddep.py
@@ -2,7 +2,12 @@ import math
 import time
 import sys
 from holobot import Holobot
-
+from collections import deque
+import numpy as np
+import imutils
+import cv2
+import os
+import RPi.GPIO as GPIO
 
 
 """-----------------------------------------------------------------------------------
@@ -12,12 +17,12 @@ Postconditions -- renvoie la distance au sol entre le robot et la balle (en cm)
 def calcDistance(diametre):
     distance = 3315.2*(diametre**-1.014)
     return distance
-    
+
 
 
 """-----------------------------------------------------------------------------------
 Préconditions  -- xBalle : type entier ; coordonnée x de la balle sur l'image
-Postconditions -- renvoie l'angle au sol entre le robot et la balle (en degrés) 
+Postconditions -- renvoie l'angle au sol entre le robot et la balle (en degrés)
 -----------------------------------------------------------------------------------"""
 def calcAngle(xBalle):
     xCentral = 320
@@ -34,10 +39,10 @@ Préconditions  -- robot : la variable du robot
 Postconditions -- initialise le robot
 -----------------------------------------------------------------------------------"""
 def initHolo(robot):
-    robot.reset_yaw()   
-    
-    
-    
+    robot.reset_yaw()
+
+
+
 """-----------------------------------------------------------------------------------
 Préconditions  -- robot : la variable du robot
                   angle : type entier ; l'angle voulu en degrés
@@ -48,20 +53,20 @@ def tournerd(robot, angle):
     dt = 0.1
     vitesse = 50
     t = 0.0
-    
+
     #Calculs :
     angle = (angle*40)/180
     temps = angle/vitesse
-    
-    
-    
+
+
+
     #Tourner :
     while(t < temps):
         robot.turn(-vitesse)
         time.sleep(dt)
         t += dt
     robot.stop_all()
-    
+
 """-----------------------------------------------------------------------------------
 Préconditions  -- robot : la variable du robot
                   angle : type entier ; l'angle voulu en degrés
@@ -72,20 +77,20 @@ def tournerg(robot, angle):
     dt = 0.1
     vitesse = 50
     t = 0.0
-    
+
     #Calculs :
     angle = (angle*40)/180
     temps = angle/vitesse
-    
-    
-    
+
+
+
     #Tourner :
     while(t < temps):
         robot.turn(vitesse)
         time.sleep(dt)
         t += dt
     robot.stop_all()
-    
+
 
 
 
@@ -100,33 +105,61 @@ def bouger(robot, distance, angle):
     dt = 0.1
     vitesse = 100
     t = 0.0
-    
+
     #Calculs :
     distance = distance*10
     temps = distance/vitesse
-    
+
     #Bouger :
     tourner(robot, angle)
     time.sleep(dt)
     while(t < temps):
         robot.move_toward(vitesse, 300)
         time.sleep(dt)
-        t += dt    
+        t += dt
     robot.stop_all()
-    
+
 holo = Holobot(sys.argv[1], 115200)
 initHolo(holo)
 
+"""-----------------------------------------------------------------------------------------------
+Préconditions   -- robot     : la variable du robot
+                   inconnu   : doit toujours être 1
+
+
+Postconditions  -- renvoie la distance du robot a un obstacle
+-----------------------------------------------------------------------------------------------"""
+def dist(robot, inconnu):
+    distance = robot.get_dist(inconnu)
+    return distance
+
+
+
+"""-----------------------------------------------------------------------------------------------
+Préconditions   -- duree    : temps durant lequel le robot continue de frapper
+                   cadence  : Nombre de coups par seconde
+
+
+Postconditions  -- Ordonne au robot de frapper
+-----------------------------------------------------------------------------------------------"""
+
+# Set up obligatoire des pins GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(3, GPIO.OUT)
+GPIO.setup(3, GPIO.OUT, initial=GPIO.HIGH)
+
+def frapper(duree, cadence):
+	t = 0
+	while t < duree :
+		GPIO.output(3, not GPIO.input(3))
+		pause = 1/cadence
+		time.sleep(pause)
+		t += pause
 
 
 
 
-from collections import deque
-import numpy as np
-import imutils
-import cv2
-import os
-
+"""--------------------------------- Début du code Principal --------------------------------- """
 
 greenLower = (5, 140, 185)
 greenUpper = (30, 215, 255)
@@ -150,7 +183,7 @@ while True:
     if len(cnts) > 0:
         c = max(cnts, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
-        
+
         if radius > 10:
             cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
             u=os.system('clear')
@@ -159,42 +192,41 @@ while True:
             print("Radius  = "+str(radius))
             pos = (300-int(x))*-1    # position par rapport au centre de l'image x=300
             print("Pos = "+str(pos))
-            
+
         else:
             u=os.system('clear')
             print("PAS DE BALLE")
             radius = -1
             x = -1
-            y = -1  
+            y = -1
     else:
         u=os.system('clear')
         print("PAS DE BALLE")
         radius = -1
         x = -1
         y = -1
-            
+
     cv2.imshow("Frame", frame)
     #cv2.imshow("Mask", mask)
-    
-    
-    if x < 250 and x !=-1:
-        #gauche
-        #tournerg(holo, 1)
-        holo.turn(5)
-        holo.move_toward(30, 300)
-        
-    elif x > 350:
-        #droite
-        #tournerd(holo,1)
-        holo.turn(-5)
-        holo.move_toward(30, 300)
-        
-    elif x ==-1:
-        holo.stop_all()
-    else:
-        holo.move_toward(30, 300)
-        
+
+    if # A METTRE ICI
+        if x < 250 and x !=-1:
+            #gauche
+            #tournerg(holo, 1)
+            holo.turn(5)
+            holo.move_toward(30, 300)
+
+        elif x > 350:
+            #droite
+            #tournerd(holo,1)
+            holo.turn(-5)
+            holo.move_toward(30, 300)
+
+        elif x ==-1:
+            holo.stop_all()
+        else:
+            holo.move_toward(30, 300)
+
     key = cv2.waitKey(1) & 0xFF
 camera.release()
 cv2.destroyAllWindows()
-
