f3b70a7285097e1ac698b4f807f8663b697ecfa4
APPASEYO <yoho64@outlook.fr>
2019-05-27 14:44:21 +0200

    Ajout du shape détector et version beta du prog qui trouverait la balle avec le shape detector

---
+++ Shape Detector/detect_circles.py
@@ -0,0 +1,72 @@
+# USAGE
+# python detect_shapes.py --image shapes_and_colors.png
+
+# import the necessary packages
+from pyimagesearch.shapedetector import ShapeDetector
+import argparse
+import imutils
+import cv2
+
+from PIL import Image
+import numpy
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to the input image")
+args = vars(ap.parse_args())
+
+# load the image and resize it to a smaller factor so that
+# the shapes can be approximated better
+image = cv2.imread(args["image"])
+resized = imutils.resize(image, width=300)
+ratio = image.shape[0] / float(resized.shape[0])
+
+# convert the resized image to grayscale, blur it slightly,
+# and threshold it
+gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+
+# find contours in the thresholded image and initialize the
+# shape detector
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+sd = ShapeDetector()
+
+elements = []
+
+# loop over the contours
+for c in cnts:
+	# compute the center of the contour, then detect the name of the
+	# shape using only the contour
+	M = cv2.moments(c)
+	cX = int((M["m10"] / M["m00"]) * ratio)
+	cY = int((M["m01"] / M["m00"]) * ratio)
+	shape = sd.detect(c)
+
+	if (shape == 'circle') :
+		# multiply the contour (x, y)-coordinates by the resize ratio,
+		# then draw the contours and the name of the shape on the image
+		c = c.astype("float")
+		c *= ratio
+		c = c.astype("int")
+		cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+
+		position = (cX, cY)
+		elements.append(position)
+
+		# show the output image
+		cv2.imshow("Image", image)
+	cv2.waitKey(0)
+
+print(elements)
+
+for i in range(len(elements)) :
+	x,y = elements[i]
+	pix = image[y, x] #output en B G R
+	print(pix)
+	if pix == () #AJOUTER LE TRESHOLD ORANGE DE LA balle
+		BALLE = True
+		xballe = image[x]
+		yballe = image[y]
